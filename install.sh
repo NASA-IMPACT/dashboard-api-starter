2d25d48713b5b3fdd3c5ed6fda607d9bed973ba7
“slesaad” <slesaad@gmail.com>
2021-05-13 12:46:10 -0500

    Remove export aws creds statement

--- install.sh
+++ install.sh
@@ -5,10 +5,10 @@ npm install -g aws-cdk
 echo "Installing python packages (pip)"
 pip install -e ".[deploy]"
 
-echo "Setting AWS_ACCOUNT_ID amd AWS_REGION"
-# Bootstrap the AWS accont
-export AWS_ACCOUNT_ID=$(aws sts get-caller-identity | jq .Account -r)
-export AWS_REGION=$(aws configure get region)
+# echo "Setting AWS_ACCOUNT_ID and AWS_REGION"
+# # Bootstrap the AWS accont
+# export AWS_ACCOUNT_ID=$(aws sts get-caller-identity | jq .Account -r)
+# export AWS_REGION=$(aws configure get region)
 
-echo "CDK bootstrapping AWS_ACCOUNT_ID amd AWS_REGION"
+echo "CDK bootstrapping AWS_ACCOUNT_ID and AWS_REGION"
 cdk bootstrap aws://$AWS_ACCOUNT_ID/$AWS_REGION --all
